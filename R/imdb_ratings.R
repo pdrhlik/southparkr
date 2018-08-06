d8eb8882d641e1a72f48c3372204e3f5346d44ff
Patrik Drhlík <patrioscraper@gmail.com>
2018-08-06 23:08:30 +0200

    improve fetch_ratings to use official IMDB data
    It now uses the official imdb.com/interfaces page that lists several .tsv
    files that can be downloaded and parsed.
    
    The old functions are kept for now but they are not exported.
    
    The fetch_ratings function returns one more column than the previous one -
    episode IMDB ID.

--- R/imdb_ratings.R
+++ R/imdb_ratings.R
@@ -1,9 +1,89 @@
+#' Fetch IMDB ratings for all South Park episodes.
+#'
+#' Downloads and parses basic information and ratings
+#' about every South Park episode. It uses the only
+#' official way that allows you to get data directly
+#' from IMDB - \url{https://datasets.imdbws.com/}.
+#'
+#' The function downloads, parses and joins three of these files:
+#' \emph{title.episode.tsv.gz}, \emph{title.basics.tsv.gz} and \emph{title.ratings.tsv.gz}.
+#'
+#' @param force_download If \code{TRUE} (default \code{FALSE}), download new IMDB files even
+#'   if there are cached files.
+#' @param make_rds If \code{TRUE} (default \code{TRUE}), automatically saves fetched and parsed
+#'   data to \emph{ratings.rds} in a current working directory.
+#' @param delete_files If \code{TRUE} (default \code{FALSE}), deletes downloaded files after
+#'   processing.
+#'
+#' @return Data frame with following columns:
+#'   \describe{
+#'     \item{episode_imdb_id}{IMDB ID that identifies an episode, movie or TV show.}
+#'     \item{season_number}{Season number.}
+#'     \item{season_episode_number}{Episode number in a season.}
+#'     \item{episode_name}{Name of the episode.}
+#'     \item{air_date}{Year when the episode aired.}
+#'     \item{user_rating}{Weighted average of user ratings.}
+#'     \item{user_votes}{Number of user votes.}
+#'   }
+#' @export
+fetch_ratings <- function(force_download = FALSE, make_rds = TRUE, delete_files = FALSE) {
+	sp_imdb_id <- "tt0121955"
+	file_names <- c("title.episode.tsv.gz", "title.basics.tsv.gz", "title.ratings.tsv.gz")
+
+	if (force_download == TRUE | any(!file.exists(file_names))) {
+		# Roughly 18 MB (84 MB unzipped) - episode-season link
+		download.file("https://datasets.imdbws.com/title.episode.tsv.gz", destfile = "title.episode.tsv.gz")
+		# Roughly 89 MB (418 MB unzipped) - title information (parsing failures)
+		download.file("https://datasets.imdbws.com/title.basics.tsv.gz", destfile = "title.basics.tsv.gz")
+		# Roughly 4 MB - ratings
+		download.file("https://datasets.imdbws.com/title.ratings.tsv.gz", destfile = "title.ratings.tsv.gz")
+	}
+
+	episode_rel_links <- readr::read_tsv(gzfile(file_names[1]))
+	episode_information <- readr::read_delim(gzfile(file_names[2]), delim = "\t", na = "\\N", escape_double = FALSE)
+	episode_ratings <- readr::read_tsv(gzfile(file_names[3]))
+
+	result <- filter(
+		episode_rel_links,
+		parentTconst == sp_imdb_id
+	) %>%
+		left_join(
+			episode_information,
+			by = "tconst"
+		) %>%
+		left_join(
+			episode_ratings,
+			by = "tconst"
+		) %>%
+		mutate(
+			episode_imdb_id = tconst,
+			season_number = as.numeric(seasonNumber),
+			season_episode_number = as.numeric(episodeNumber),
+			episode_name = primaryTitle,
+			air_date = startYear,
+			user_rating = averageRating,
+			user_votes = numVotes
+		) %>%
+		select(
+			episode_imdb_id, season_number, season_episode_number,
+			episode_name, air_date, user_rating, user_votes
+		)
+
+	if (delete_files == TRUE) {
+		unlink(file_names)
+	}
+
+	if (make_rds == TRUE) {
+		readr::write_rds(result, "ratings.rds")
+	}
+
+	return(result)
+}
+
 #' Fetch IMDB ratings for one season.
 #'
 #' @param season_number
-#'
-#' @export
-fetch_season_ratings <- function(season_number) {
+fetch_season_ratings_old <- function(season_number) {
 	rating_url <- paste0("https://www.imdb.com/title/tt0121955/episodes?season=", season_number)
 	html <- xml2::read_html(rating_url)
 
@@ -33,8 +113,7 @@ fetch_season_ratings <- function(season_number) {
 #' Fetch ratings for all selected seasons.
 #'
 #' @param season_numbers
-#' @export
-fetch_ratings <- function(season_numbers) {
+fetch_ratings_old <- function(season_numbers) {
 	result <- purrr::map_df(season_numbers, fetch_season_ratings)
 
 	return(result)
